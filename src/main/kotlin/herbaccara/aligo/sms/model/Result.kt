d8c8ae0abd854bde4dd7a04941c70403399cd23c
이용홍 <allbegray.unimewo@gmail.com>
2023-02-27 14:41:41 +0900

    :wastebasket: Result.kt @Deprecated 처리

--- src/main/kotlin/herbaccara/aligo/sms/model/Result.kt
+++ src/main/kotlin/herbaccara/aligo/sms/model/Result.kt
@@ -2,6 +2,7 @@ package herbaccara.aligo.sms.model
 
 import com.fasterxml.jackson.annotation.JsonProperty
 
+@Deprecated("성공이면 필드 정보가 필요없고, 실패면 throw AligoSmsResponseException(resultCode, message) 하기 때문에 사용할 이유가 없다.")
 data class Result(
     @field:JsonProperty("result_code")
     val resultCode: Int,
